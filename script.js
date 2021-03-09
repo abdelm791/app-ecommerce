c2ac4288767d3b21ceeb154910fcbf06029d54b0
Glitch (hello-webpage) <none>
2021-03-09 17:53:41 +0000

    💼🎡 Checkpoint ./script.js:29480214/181 ./Includes/header.html:29480214/416 ./index.html:29480214/584

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-$(function () {
-  var includes = $('[data-include]')
-  $.each(includes, function () {
-    var file = 'Includes/' + $(this).data('include') + '.html'
-    $(this).load(file)
-  })
-}) ;
+
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
