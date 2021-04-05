c749ed031f8539c85f1ab476605f0156acbbc726
Glitch (hello-webpage) <none>
2021-04-05 11:59:23 +0000

    🚠🚈 Checkpoint ./Pages/products.html:29480214/1241 ./script.js:29480214/395

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+$("#addit").click(function(){
+  var productId = $("#productId").val();
+  var productName = $("#productName").val();
+  var productQuantity = $("#productQuantity").val();
+  var data = {
+    'product_id': productId,
+    'product_name': productName,
+    'quantity': productQuantity
+  };
 
+  $.post("/cart/add", data, showDone);
+ });
+ 
+ var showDone = function() {
+  /* Make something happen here*/
+ }
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
