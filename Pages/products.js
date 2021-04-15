908c94e5488f2dc0596cf1d60257a492448c492c
Glitch (hello-webpage) <none>
2021-04-15 19:02:20 +0000

    🏹👢 Checkpoint ./Pages/products.js:29480214/1201

--- Pages/products.js
+++ Pages/products.js
@@ -8,31 +8,31 @@ var products = {
   },
   124: {
     name : "Huawei P40",
-    desc : "Unsatiable its possession nor off.",
+    desc : "Released 2020, April 07 175g, 8.5mm thickness Android 10, EMUI 10.1 128GB/256GB storage, NM",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fhuawei-p40-pro.png?v=1615326187567",
     price : 600
   },
   125: {
     name : "Galaxy Ultra S21",
-    desc : "All difficulty unreserved the solicitude.",
+    desc : "Released 2021, January 29 227g (Sub6), 229g (mmWave), 8.9mm thickness Android 11, One UI 3.1 128GB/256GB/512GB storage, no card slot",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fsamsung-galaxy-s20-ultra-5g.png?v=1615326692569",
     price : 700
   },
   126: {
     name : "Motorola One Hyper",
-    desc : "Had judgment out property the supplied. ",
+    desc : "Released 2020, January 22 210g, 8.9mm thickness Android 10, up to Android 11 128GB storage, microSDXC",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fmotorola-one-hyper.png?v=1615328961903",
     price : 300
   },
   127: {
     name : "Motorola One Macro",
-    desc : "Had judgment out property the supplied. ",
+    desc : "Released 2019, October 12 186g, 9mm thickness Android 9.0, up to Android 10 64GB storage, microSDXC",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fmotorola-one-macro.png?v=1615329621316",
     price : 229
   },
 128: {
     name : "Xiaomi Mi 10",
-    desc : "Had judgment out property the supplied. ",
+    desc : "Released 2020, February 14 208g, 9mm thickness Android 10, up to Android 11, MIUI 12.5 128GB/256GB storage, no card slot ",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fxiaomi-mi-10.png?v=1615329745777",
     price : 399
   },
@@ -44,19 +44,19 @@ var products = {
   },
 130: {
     name : "Xiaomi Mi A3",
-    desc : "Had judgment out property the supplied. ",
+    desc : "Released 2019, July 24 173.8g, 8.5mm thickness Android 9.0, up to Android 11, Android One 64GB/128GB storage, microSDXC ",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fxiaomi-mi-a3.png?v=1615330755874",
     price : 208
   },
 131: {
     name : "Xiaomi Redmi Note 9 Pro",
-    desc : "Had judgment out property the supplied. ",
+    desc : "",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fxiaomi-redmi-note-9-pro.png?v=1615330865071",
     price : 229
   },
 132: {
     name : "Xiaomi Mi 10 lite",
-    desc : "Had judgment out property the supplied. ",
+    desc : "Released 2020, May 25 192g, 7.9mm thickness Android 10, MIUI 12 64GB/128GB/256GB storage, no card slot ",
     img : "https://cdn.glitch.com/dd0e54bb-605f-41b2-ada5-7027b3736516%2Fxiaomi-mi-10-lite.png?v=1615330683502",
     price : 306
   },
